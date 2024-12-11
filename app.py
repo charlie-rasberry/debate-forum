2ea6e3a0eaf78942ee2cc7aa7e9bf823d9b2762c
chg <charlie.rasberry@outlook.com>
2024-12-11 04:43:22 +0000

    Ready to start.

--- app.py
+++
@@ -1,4 +0,0 @@
-from flask import Flask, render_template, request, jsonify, session, redirect, url_for
-
-app = Flask(__name__)
-app.secret_key = 'your_secret_key'
---
+++ schema.py
@@ -0,0 +1,7 @@
+import sqlite3
+db = sqlite3.connect("debate.sqlite")
+cursor = db.cursor()
+with open("5013dbinit.sql") as f:
+   cursor.executescript(f.read())
+db.commit()
+db.close()
